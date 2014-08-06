1f62db23cf1f4d9f0be4bd3af167b2dfd12e2b0a
Lev Babchenko <lev@3divi.com>
2014-08-06 13:37:43 +0600

    fn-in-fn

--- merge.lua
+++ merge.lua
@@ -54,49 +54,45 @@ function db_load(fname)
 end;
 
 
-function spc(lvl)
-    local s = '';
-    for i = 1, lvl do
-        s = s .. '\t';
-    end;
-    return s;
-end;
-
-
-function pprint(v,lvl)
-    local t = type(v);
-    if t == 'nil' then
-        return nil;
-    elseif t == 'boolean' then
-        return tostring(v);
-    elseif t == 'string' then
-        return '"'..v..'"';
-    elseif t == 'number' then
-        return tostring(v);
-    elseif t == 'table' then
-        local s = '{\n';
-        local keys = {};
-        for key, _ in pairs(v) do
-            table.insert(keys, key);
-        end;
-        table.sort(keys,function(a,b) return a < b end);
-        for ix = 1, #keys do
-            key = keys[ix];
-            val = v[key];
-            s = s .. spc(lvl) ..
-                '[' .. pprint(key,0) .. '] = ' ..
-                pprint(val, lvl + 1) ..
-                ',\n';
-        end;
-        s = s .. spc(lvl-1) .. '}';
-        return s;
-    else
-        error("unprocessible type ".. t);
-    end;
-end;
-
-
 function db_save(fname, data, config)
+    function pprint(v,lvl)
+        function spc(lvl)
+            local s = '';
+            for i = 1, lvl do
+                s = s .. '\t';
+            end;
+            return s;
+        end;
+        local t = type(v);
+        if t == 'nil' then
+            return nil;
+        elseif t == 'boolean' then
+            return tostring(v);
+        elseif t == 'string' then
+            return '"'..v..'"';
+        elseif t == 'number' then
+            return tostring(v);
+        elseif t == 'table' then
+            local s = '{\n';
+            local keys = {};
+            for key, _ in pairs(v) do
+                table.insert(keys, key);
+            end;
+            table.sort(keys, function(a,b) return a < b end);
+            for ix = 1, #keys do
+                key = keys[ix];
+                val = v[key];
+                s = s .. spc(lvl) ..
+                    '[' .. pprint(key,0) .. '] = ' ..
+                    pprint(val, lvl + 1) ..
+                    ',\n';
+            end;
+            s = s .. spc(lvl-1) .. '}';
+            return s;
+        else
+            error("unprocessible type ".. t);
+        end;
+    end; -- pprint
     print("save " .. num_nodes (data) .. " nodes to "..fname);
     local f = io.open(fname, 'w');
     f:write('GatherItems = ' .. pprint(data,1)..'\n');
@@ -105,62 +101,62 @@ function db_save(fname, data, config)
 end;
 
 
-function is_same_node(a, b)
-    return ((a.x - b.x)^2 + (a.y - b.y)^2) < mindist^2;
-end;
-
-
-function merge_nodeset(a, b)
-    local r = {};
-    for _, v in pairs(a) do
-        r[#r+1] = v;
-        num_total = num_total + 1;
-    end;
-    for _, v in pairs(b) do
-        local found = false;
-        for _, vr in pairs(r) do
-            if is_same_node(v, vr) then
-                found = true;
-                break;
-            end;
-        end;
-        if found then
-            num_skipped = num_skipped + 1;
-        else
-            r[#r+1] = v;
-            num_added = num_added + 1;
-            num_total = num_total + 1;
-        end;
-    end;
-    return r;
-end;
-
-
-function merge_zone(a, b)
-    local r = {};
-    for k, v in pairs(a) do
-        r[k] = v;
-    end;
-    for k, v in pairs(b) do
-        r[k] = merge_nodeset(r[k] or {}, v);
-    end;
-    return r;
-end;
-
-
-function merge_continent(a, b)
-    local r = {};
-    for k, v in pairs(a) do
-        r[k] = v;
-    end;
-    for k, v in pairs(b) do
-        r[k] = merge_zone(r[k] or {}, v);
-    end;
-    return r;
-end;
-
-
 function merge_base(a, b)
+
+    function merge_continent(a, b)
+
+        function merge_zone(a, b)
+
+            function merge_nodeset(a, b)
+
+                function is_same_node(a, b)
+                    return ((a.x - b.x)^2 + (a.y - b.y)^2) < mindist^2;
+                end; -- is_same_node
+
+                local r = {};
+                for _, v in pairs(a) do
+                    r[#r+1] = v;
+                    num_total = num_total + 1;
+                end;
+                for _, v in pairs(b) do
+                    local found = false;
+                    for _, vr in pairs(r) do
+                        if is_same_node(v, vr) then
+                            found = true;
+                            break;
+                        end;
+                    end;
+                    if found then
+                        num_skipped = num_skipped + 1;
+                    else
+                        r[#r+1] = v;
+                        num_added = num_added + 1;
+                        num_total = num_total + 1;
+                    end;
+                end;
+                return r;
+            end; -- merge_nodeset
+
+            local r = {};
+            for k, v in pairs(a) do
+                r[k] = v;
+            end;
+            for k, v in pairs(b) do
+                r[k] = merge_nodeset(r[k] or {}, v);
+            end;
+            return r;
+        end; -- merge_zone
+
+        local r = {};
+        for k, v in pairs(a) do
+            r[k] = v;
+        end;
+        for k, v in pairs(b) do
+            r[k] = merge_zone(r[k] or {}, v);
+        end;
+        return r;
+    end; -- merge_continent
+
     num_total,num_added,num_skipped = 0,0,0
     local r = {};
     for k, v in pairs(a) do
@@ -169,12 +165,12 @@ function merge_base(a, b)
     for k, v in pairs(b) do
         r[k] = merge_continent(r[k] or {}, v);
     end;
-    print("merge results: total=" .. num_total ..
+    print("merge results: compared=" .. num_total ..
         ", added=" .. num_added ..
         ", skipped="..num_skipped..
-        ", num="..num_nodes(r));
+        ", size="..num_nodes(r));
     return r;
-end;
+end; -- merge_base
 
 
 ---------------------------------------------------
